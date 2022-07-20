6352911b61279997ff2026f26c2472f6363e10d6
Nate <nathanial.lunceford@gmail.com>
2022-07-20 11:25:58 -0500

    Initial Commit

---
+++ MethodOverloadingExercise/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Program
+{
+    public static int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public static decimal Add(decimal num1, decimal num2)
+    {
+        return num1 + num2;
+    }
+
+    public static string Add(int num1, int num2, bool isMoney)
+    {
+        if(isMoney)
+        {
+            if (num1 + num2 == 1)
+            {
+                return $"{num1 + num2} dollar";
+            }
+            else
+            {
+                return $"{num1 + num2} dollars";
+            }
+        }
+        else
+        {
+            return $"{num1 + num2}";
+        }
+        
+    }
+
+    private static void Main(string[] args)
+    {
+
+    }
+}
